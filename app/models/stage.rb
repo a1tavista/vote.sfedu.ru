bad5ef5d172d570be31e36f75ea04d5b51b188ac
Andrew Rudenets <ar@elonsoft.ru>
2017-08-06 23:15:21 +0300

    Add method for picking current semester

--- app/models/stage.rb
+++ app/models/stage.rb
@@ -1,4 +1,12 @@
 class Stage < ApplicationRecord
   has_and_belongs_to_many :semesters
   has_and_belongs_to_many :questions
+
+  def self.current
+    current_time = Time.current
+    Stage
+      .where('stages.starts_at >= ?', current_time)
+      .where('stages.ends_at <= ?', current_time)
+      .first
+  end
 end
