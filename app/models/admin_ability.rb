049003b9118af7adfe902e0f748529ce0dee6bda
Andrew Rudenets <ar@elonsoft.ru>
2018-03-02 08:49:38 +0300

    Don't give access to guests

--- app/models/admin_ability.rb
+++ app/models/admin_ability.rb
@@ -2,7 +2,7 @@ class AdminAbility
   include CanCan::Ability
 
   def initialize(user)
-    return if user.present? || !user.admin?
+    return if user.nil? || !user.admin?
 
     can :manage, :all
     # Define abilities here. More info: https://github.com/CanCanCommunity/cancancan/wiki/defining-abilities
