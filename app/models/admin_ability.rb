0567b3f8b7773662f13b45b0990c161363dfdc73
Andrew Rudenets <ar@elonsoft.ru>
2018-03-02 08:32:54 +0300

    Fix issue with admin access for guest

--- app/models/admin_ability.rb
+++ app/models/admin_ability.rb
@@ -1,8 +1,8 @@
 class AdminAbility
   include CanCan::Ability
 
-  def initialize(_user)
-    return unless _user.admin?
+  def initialize(user)
+    return if user.present? || !user.admin?
 
     can :manage, :all
     # Define abilities here. More info: https://github.com/CanCanCommunity/cancancan/wiki/defining-abilities
