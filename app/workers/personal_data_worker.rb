96fa620018fd16ac26fff9772fe080abe8646170
Andrew Rudenets <ar@elonsoft.ru>
2018-12-20 12:47:05 +0300

    Don't retry PersonalDataWorker

--- app/workers/personal_data_worker.rb
+++ app/workers/personal_data_worker.rb
@@ -1,5 +1,6 @@
 class PersonalDataWorker
   include Sidekiq::Worker
+  sidekiq_options queue: :default, retry: false
 
   def perform(student_id)
     Student.find_by_id(student_id)&.load_personal_information!
