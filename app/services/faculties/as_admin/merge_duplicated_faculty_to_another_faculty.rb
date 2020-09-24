1f6461ce779e80994a392102a56f80fb26242b16
Andrew Rudenets <andrey.rudenets@gmail.com>
2020-09-24 19:52:00 +0300

    Implement backend for managing faculty list

---
+++ app/services/faculties/as_admin/merge_duplicated_faculty_to_another_faculty.rb
@@ -0,0 +1,44 @@
+module Faculties
+  module AsAdmin
+    class MergeDuplicatedFacultyToAnotherFaculty
+      include Dry::Transaction
+
+      class Contract < Dry::Validation::Contract
+        config.messages.backend = :i18n
+
+        params do
+          required(:faculty).filled(type?: Faculty)
+          required(:duplicated_faculty).filled(type?: Faculty)
+        end
+      end
+
+      step :validate_input
+      check :faculties_are_different
+      step :merge_faculties
+      step :destroy_duplicated_faculty
+
+      def validate_input(input)
+        ::Operations::ValidateInput.new.call(input, contract_klass: Contract)
+      end
+
+      def faculties_are_different(input)
+        input[:faculty].id != input[:duplicated_faculty].id
+      end
+
+      def merge_faculties(faculty:, duplicated_faculty:)
+        Poll::FacultyParticipant.where(faculty_id: duplicated_faculty.id).update_all(faculty_id: faculty.id)
+        GradeBook.where(faculty: duplicated_faculty).update_all(faculty_id: faculty.id)
+
+        faculty.aliases ||= []
+        faculty.aliases << duplicated_faculty.name
+        faculty.save ? Success(faculty: faculty, duplicated_faculty: duplicated_faculty) : Failure()
+      end
+
+      def destroy_duplicated_faculty(faculty:, duplicated_faculty:)
+        duplicated_faculty.destroy
+
+        Success(faculty: faculty)
+      end
+    end
+  end
+end
---
+++ spec/services/faculties/as_admin/merge_duplicated_faculty_to_another_faculty_spec.rb
@@ -0,0 +1,31 @@
+require "rails_helper"
+
+describe Faculties::AsAdmin::MergeDuplicatedFacultyToAnotherFaculty do
+  subject { described_class.new.call(faculty: master_faculty, duplicated_faculty: duplicated_faculty) }
+
+  let!(:master_faculty) { create(:faculty) }
+  let!(:duplicated_faculty) { create(:faculty) }
+  let!(:students) { create_list(:student, 3, :with_grade_book, faculty_id: duplicated_faculty.id) }
+  let!(:polls) { create_list(:poll, 3, faculty_ids: [duplicated_faculty.id]) }
+
+  context "with valid params" do
+    it("returns success") { expect(subject).to be_success }
+    it("changes faculty for students") do
+      expect { subject }.to change { students.map(&:reload).map(&:faculty_ids).flatten }
+                              .from([duplicated_faculty.id] * 3).to([master_faculty.id] * 3)
+    end
+
+    it("changes faculties-participants in poll") do
+      expect { subject }.to change { polls.map(&:reload).map(&:faculty_ids).flatten }
+                              .from([duplicated_faculty.id] * 3).to([master_faculty.id] * 3)
+    end
+
+    it("adds alias to master faculty") do
+      expect { subject }.to change { master_faculty.aliases }.from(nil).to([duplicated_faculty.name])
+    end
+
+    it("destroys duplicated faculty") do
+      expect { subject }.to change(Faculty, :count).by(-1)
+    end
+  end
+end
