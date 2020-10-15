2dfdccf18b0361edd88d5562a53d37c2befd4b93
Andrew Rudenets <andrey.rudenets@gmail.com>
2020-10-15 17:11:33 +0300

    Fix issue with events dispatching

--- app/handlers/concerns/async_handler.rb
+++ app/handlers/concerns/async_handler.rb
@@ -2,7 +2,7 @@ module AsyncHandler
   def perform(payload)
     event = Rails.configuration.event_store.deserialize(payload.transform_keys(&:to_sym))
 
-    Raven.tags_context(dispatcher: self.class.to_s, event_type: event.type)
+    Raven.tags_context(dispatcher: self.class.to_s, event_type: event.event_type)
     Raven.extra_context(event: event.to_h)
 
     super(event)
